899e600a27fa165570f859ac9c5756b8f60177b8
damscot <damscot@users.noreply.github.com>
2017-02-26 02:54:51 +0100

    touche clavier fonctionnelles

--- balance_jeanne/balance_jeanne.ino
+++ balance_jeanne/balance_jeanne.ino
@@ -1,12 +1,14 @@
 #include <MenuSystem.h>
+#include <avr/pgmspace.h>
 #include <Encoder.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <Keypad_I2Ca.h>
 #include <Keypad.h>
 
-
+// Rotary Encoder Global Position
 long newPosition;
+uint8_t idx_art;
 
 #define ENCFORW_PIN 3
 #define ENCBACKW_PIN 2
@@ -24,17 +26,129 @@ LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars
 
 uint8_t ConfValue = 0;
 
+/***********************************/
+/*             ARTICLES            */
+/***********************************/
+
+int current_art_pu_cent_kg;
+
+typedef enum famille_art {
+  MANUEL=0,
+  LEGUMES,
+  FRUITS,
+  FROMAGES,
+  CHARCUTERIES,
+} famille_art_t;
+
+typedef struct articles
+{
+  uint8_t code;
+  char name[20];
+  int pu_cent_kg;
+} articles_t;
+
+static const articles_t PROGMEM manuel[] = {
+  {-1,NULL,0},
+};
+
+static const articles_t PROGMEM legumes[] = {
+  {0,"Pomme de Terre Bio",235},
+  {1,"Tomate Bio",211},
+  {2,"Poireau",245},
+  {3,"Carotte",235},
+  {-1,NULL,0},
+};
+
+static const articles_t PROGMEM fruits[] = {
+  {0,"Pomme Bio",135},
+  {1,"Poire",311},
+  {2,"Ananas",275},
+  {-1,NULL,0},
+};
+
+static const articles_t PROGMEM fromages[] = {
+  {0,"Tomme de Savoie",235},
+  {1,"Gruyere",211},
+  {2,"Emmental",245},
+  {3,"Gouda",235},
+  {4,"Bleu Auvergne",235},
+  {5,"Chevre Schudeler",235},
+  {-1,NULL,0},
+};
+
+static const articles_t PROGMEM charcuteries[] = {
+  {0,"Salami",135},
+  {1,"Saucisse Ardeche",311},
+  {2,"Jambon",275},
+  {-1,NULL,0},
+};
+
+typedef struct famille
+{
+  uint8_t size;
+  const articles_t * table;
+} famille_t;
+
+static const famille_t art_table [] = {
+  [MANUEL] =  {sizeof(manuel)/sizeof(articles_t),manuel},
+  [LEGUMES] =  {sizeof(legumes)/sizeof(articles_t),legumes},
+  [FRUITS] = {sizeof(fruits)/sizeof(articles_t),fruits},
+  [FROMAGES] = {sizeof(fromages)/sizeof(articles_t),fromages},
+  [CHARCUTERIES] = {sizeof(charcuteries)/sizeof(articles_t),charcuteries},
+};
+
+void Display_Art(uint8_t fam, uint8_t idx) {
+  if ((fam > MANUEL) && (fam <= CHARCUTERIES) && (idx < art_table[fam].size)) {
+    char str[20];
+    articles_t art;
+    char * src = (char *)(&(art_table[fam].table[idx]));
+    char * dest= (char *)(&art);
+    
+    for (int i = 0; i < sizeof(art); i++)
+    {
+      *dest++ = pgm_read_byte_near(src++);
+    }
+    
+    sprintf(str, "%d,%d,%d   ",fam, idx, art_table[fam].size);
+    lcd.setCursor(0,1);
+    lcd.print(str);   
+    sprintf(str, "%s",art.name);
+    lcd.setCursor(0,2);
+    lcd.print(str);
+    
+    current_art_pu_cent_kg = art.pu_cent_kg;
+    sprintf(str, "Prix: %d.%02d  Euro   ",art.pu_cent_kg/100 ,art.pu_cent_kg%100);
+    lcd.setCursor(0,3);
+    lcd.print(str);
+  }
+  else {
+    idx_art = 0;
+  }
+}
+
+/***********************************/
+/*             MENU UI             */
+/***********************************/
+
 // Menu variables
 MenuSystem ms;
 Menu mm("Main Menu");
-MenuItem mm_mi1("Menu 1");
-MenuItem mm_mi2("Menu 2");
-Menu mu1("Configuration");
-MenuItem mu1_mi1("Show current");
-MenuItem mu1_mi2("Set value");
-MenuItem mu1_mi3("Go back");
+MenuItem mm_mi1("* MANUEL");
+Menu mu1("=> LEGUMES");
+MenuItem mu1_mi1("* LEGUMES");
+Menu mu2("=> FRUITS");
+MenuItem mu2_mi1("* FRUITS");
+Menu mu3("=> FROMAGES");
+MenuItem mu3_mi1("* FROMAGES");
+Menu mu4("=> CHARCUTERIE");
+MenuItem mu4_mi1("* CHARCUTERIE");
+Menu muc("Configuration");
+MenuItem muc_mi1("Show current");
+MenuItem muc_mi2("Set value");
+MenuItem muc_mi3("Go back");
 
 #define SELECTED_DISPLAY_DELAY 500
+
 boolean menuSelected = false; // no menu item has been selected
 boolean commandReceived = false;// no command has been received (Seria, button, etc)
 enum setMenuSelected_Type { 
@@ -42,6 +156,28 @@ enum setMenuSelected_Type {
 setMenuSelected_Type setMenu;
 byte cursorPosition;
 String setString;
+
+// Initialize Menu
+void setup_menu(void) {
+  menuSelected = false;
+  mm.add_item(&mm_mi1, &on_item1_selected);
+  mm.add_menu(&mu1);
+  mu1.add_item(&mu1_mi1, &on_back_selected);
+  mm.add_menu(&mu2);
+  mu2.add_item(&mu2_mi1, &on_back_selected);
+  mm.add_menu(&mu3);
+  mu3.add_item(&mu3_mi1, &on_back_selected);
+  mm.add_menu(&mu4);
+  mu4.add_item(&mu4_mi1, &on_back_selected);
+   
+  mm.add_menu(&muc);
+  muc.add_item(&muc_mi1, &on_value_show_selected);
+  muc.add_item(&muc_mi2, &on_value_set_selected);
+  muc.add_item(&muc_mi3, &on_back_selected);
+  ms.set_root_menu(&mm);
+  displayMenu();
+}
+
 
 // Menu callback functions
 void on_item1_selected(MenuItem* p_menu_item) {
@@ -281,17 +417,17 @@ byte colPins_out[COLS] = {6, 5, A1}; //connect to the column pinouts of the keyp
 int search_keys_out(char in, char* out, int *row, int *col)
 {
   int i,j;
-  Serial.print("Search : ");
-  Serial.println(in);
+  //Serial.print("Search : ");
+  //Serial.println(in);
   for(i = 0; i < ROWS; i++)
   {
     for(j = 0; j < COLS; j++)
     {
       if (in == keys_out[i][j]) {
-        Serial.print("Found: ");
-        Serial.print(i);
-        Serial.print(" , ");
-        Serial.println(j);
+        //Serial.print("Found: ");
+        //Serial.print(i);
+        //Serial.print(" , ");
+        //Serial.println(j);
         *row = i;
         *col =j;
         *out = keys_out_remap[i][j];
@@ -305,7 +441,7 @@ int search_keys_out(char in, char* out, int *row, int *col)
   return 0;
 }
 
-char send_keys_out(char in)
+char send_keys_out(char in, int to_balance)
 {
       int row,col,col_pin;
       unsigned long temp;
@@ -313,9 +449,9 @@ char send_keys_out(char in)
       char out = 0;
       
       if (search_keys_out(in, &out, &row, &col)) {
-        /* get column pin */
         col = colPins_out[col];
-        while (1) {
+        /* get column pin */
+        while (to_balance) {
         /* pin 8 is Row 3 (2,5,8 character) is is a synchro signal */
         temp = pulseIn(8, HIGH, 110000);
         if (temp > 50000) {
@@ -331,8 +467,8 @@ char send_keys_out(char in)
           digitalWrite(col,HIGH);
           delayMicroseconds(keys_out_length[row][2]);
           digitalWrite(col,LOW);
-          Serial.print("Send Key: ");
-          Serial.println(in);
+          //Serial.print("Send Key: ");
+          //Serial.println(in);
           /*Serial.print("Value: ");
           Serial.print(row);
           Serial.print(": ");
@@ -352,6 +488,25 @@ char send_keys_out(char in)
       return out;
 }
 
+void send_number_out(int val)
+{
+  int i = 0;
+  char number[6];
+
+  while ((i < 6) && val) {
+    //send_keys_out('0'+(val%10),1);
+    number[i++] = '0' + val%10;
+    val = ((val - val%10)/10);
+  }
+  
+  send_keys_out('C',1);
+  delay(50);
+  while (i > 0) {
+    send_keys_out(number[--i],1);
+    delay(50);
+  }
+}
+
 /***********************************/
 /*               SETUP             */
 /***********************************/
@@ -365,8 +520,6 @@ void setup() {
   lcd.init();
   // Print a message to the LCD.
   lcd.backlight();
-  lcd.setCursor(3,0);
-  lcd.print("Basic Encoder");
 
   pinMode(ENCSW_PIN, INPUT);
   digitalWrite(ENCSW_PIN, HIGH);
@@ -385,16 +538,9 @@ void setup() {
     pinMode(colPins_out[i],OUTPUT);
     digitalWrite(6,LOW);
   }
+  
+  setup_menu();
 
-  menuSelected = false;
-  mm.add_item(&mm_mi1, &on_item1_selected);
-  mm.add_item(&mm_mi2, &on_item2_selected);
-  mm.add_menu(&mu1);
-  mu1.add_item(&mu1_mi1, &on_value_show_selected);
-  mu1.add_item(&mu1_mi2, &on_value_set_selected);
-  mu1.add_item(&mu1_mi3, &on_back_selected);
-  ms.set_root_menu(&mm);
-  displayMenu();
 }
 
 /***********************************/
@@ -406,6 +552,7 @@ void loop() {
   char inputKey = customKeypad.getKey();
   char outputKey;
   char poids[10], pu[10], prix[10];
+  const Menu * curent_menu;
     
   char sw0,sw1,encsw;
   newPosition = (Enc.read())/4;
@@ -413,7 +560,7 @@ void loop() {
   if (newPosition != oldPosition) {
     if (newPosition > oldPosition) {
       if(!menuSelected) {
-        Serial.println("goto next");      
+        idx_art ++;      
         ms.next();
       }
       else {
@@ -428,8 +575,8 @@ void loop() {
     }
     else {
       if(!menuSelected) {
-        Serial.println("goto prev");
         ms.prev();
+        idx_art --;
       }
       else {
         if(setMenu == VALUE_SET) {
@@ -465,24 +612,32 @@ void loop() {
   
   //SELECTED_DISPLAY_DELAY
   updateDisplay();
+  curent_menu = ms.get_current_menu();
   
   if(!menuSelected) {
     
     if (inputKey){
-      outputKey = send_keys_out(inputKey);
+      outputKey = send_keys_out(inputKey, (curent_menu == &mm));
       sprintf(str, "Key:%c   ",outputKey);
       lcd.setCursor(3,1);
       lcd.print(str);
-      if (outputKey == '=')
+      if (outputKey == '=') {
+        send_number_out(current_art_pu_cent_kg);
+        delay(100);
         get_serial_balance(poids,pu,prix);
+      }
     }
+    
+    
+    if ((curent_menu != &mm) && (curent_menu != &muc))
+      Display_Art(mm.get_cur_menu_component_num(),idx_art);
+    else {
     sprintf(str, "Val:%d   ",newPosition);
     lcd.setCursor(3,2);
     lcd.print(str);
-    sprintf(str, "SW:%d  ",encsw);
+    sprintf(str, "SW:%d  ", ms.get_current_menu());
     lcd.setCursor(0,3);
     lcd.print(str);
-  }
-  
-  
+    }
+  } 
 }
