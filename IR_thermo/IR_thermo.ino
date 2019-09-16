6aaa7975f483ec78feab26c7b53aa0ca8f1c2098
damscot <damscot@users.noreply.github.com>
2019-09-17 00:19:37 +0200

    nouveau boitier IR_Thermo
    boitier pla impression 3D IR Thermo

--- IR_thermo/IR_thermo.ino
+++ IR_thermo/IR_thermo.ino
@@ -4,7 +4,16 @@
 #include "U8glib.h"
 
 #define LASER_PIN     6
-#define BUTTON_PIN    12  
+#define BUTTON1_PIN    11  
+#define BUTTON2_PIN    12
+
+#ifdef DEBUG
+#define DGB_Print(...) Serial.Print(...)
+#define DGB_Println() Serial.Println()
+#else
+#define DGB_Print(...)
+#define DGB_Println()
+#endif
 
 uint8_t count;
 uint8_t laser_strength[4] = {0,1,32,255};
@@ -26,8 +35,10 @@ U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE|U8G_I2C_OPT_DEV_0);	// I2C / TWI
 
 void setup() {
   Serial.begin(115200);
-  pinMode(BUTTON_PIN, INPUT);
-  digitalWrite(BUTTON_PIN, HIGH);
+  pinMode(BUTTON1_PIN, INPUT);
+  digitalWrite(BUTTON1_PIN, HIGH);
+  pinMode(BUTTON2_PIN, INPUT);
+  digitalWrite(BUTTON2_PIN, HIGH);
   
   // initialize the lcd 
   lcd.init();
@@ -57,29 +68,28 @@ void draw(char * str, char * str2, char * str3) {
 void loop() {
   unsigned long time = millis();
   
-  if ((digitalRead(BUTTON_PIN) == 0) && (skip_next == 0)) {
-    delay(200);
-    if (digitalRead(BUTTON_PIN) == 0) {
-      delay(1000);
-      Serial.print("SAVING");
+  if (digitalRead(BUTTON1_PIN) == 0) {
+    delay(10);
+    if (digitalRead(BUTTON1_PIN) == 0) {
+      DGB_Print("SAVING");
       amb_sav = amb;
       obj_sav = obj;
       skip_next = 1;
     }
-    else {
+  }
+
+  if (digitalRead(BUTTON2_PIN) == 0) {
+    delay(100);
+    if (digitalRead(BUTTON2_PIN) == 0) {
+      delay(10);
       count++;
       if (count >= 4) {
         count = 0;
       }
-      Serial.print("LASER MODE = ");
-      Serial.print(count);
-      Serial.println();
-    }
-  }
-  else {
-    if ((digitalRead(BUTTON_PIN) == 1) && (skip_next == 1)) {
-      delay(100);
-      skip_next = 0;
+      DGB_Print("LASER MODE = ");
+      DGB_Print(count);
+      DGB_Println();
+      skip_next = 1;
     }
   }
   
@@ -91,12 +101,12 @@ void loop() {
     snprintf(str,20," %03d.%d  |  %03d.%d ",amb/10, amb%10, obj/10, obj%10);
     snprintf(str2,20," %03d.%d  |  %03d.%d ",amb_sav/10, amb_sav%10, obj_sav/10, obj_sav%10);
     snprintf(str3,20,"%c%03d.%d  | %c%03d.%d",((amb-amb_sav<0) ? '-':'+'), abs((amb-amb_sav)/10), abs((amb-amb_sav)%10), ((obj-obj_sav<0) ? '-':'+'), abs((obj-obj_sav)/10), abs((obj-obj_sav)%10));
-    Serial.print(str);
-    Serial.println();
-    Serial.print(str2);
-    Serial.println();
-    Serial.print(str3);
-    Serial.println();
+    DGB_Print(str);
+    DGB_Println();
+    DGB_Print(str2);
+    DGB_Println();
+    DGB_Print(str3);
+    DGB_Println();
     lcd.setCursor(0,1);
     lcd.print(str);
     lcd.setCursor(0,2);
@@ -104,12 +114,14 @@ void loop() {
     lcd.setCursor(0,3);
     lcd.print(str3);
     analogWrite(LASER_PIN, laser_strength[count]);
-    Serial.println();
+    DGB_Println();
   }
   
   u8g.firstPage();  
   do {
     draw(str,str2,str3);
   } while( u8g.nextPage() );
-  
+
+  if (skip_next == 1)
+    delay(250);
 }
